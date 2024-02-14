3973f725c6de7a9a9de55b887abc9460fc4fc210
julien-turck <157139199+julien-turck@users.noreply.github.com>
2024-02-14 09:46:21 +0100

    recupe form fields

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 let btn = document.querySelector('button');
 btn.addEventListener('click', function(event){
+  //récupération des champs du formulaire
+  let newFirstName = document.getElementById("first-name");
+  let newName = document.getElementById("last-name");
+  let newMessage = document.getElementById("message");

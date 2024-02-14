ae87b9e82999901941d9fcc8376b6e873937af06
julien-turck <157139199+julien-turck@users.noreply.github.com>
2024-02-14 09:54:13 +0100

    reassignment of old information to the starting block

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ btn.addEventListener('click', function(event){
   let item1 = document.getElementById("blocNiv-1");
   var sousBloc = item1.cloneNode(true);
   blocPrincipal.appendChild(sousBloc);
+
+  //réattribution des anciennes informations au bloc de départ
+  let ancientComment = document.getElementById("commentaire");
+  ancientComment.innerHTML = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae accusantium quae doloribus. Nobis ipsam libero asperiores a velit ab nemo facere molestias quisquam. Libero, repudiandae dignissimos. Doloribus hic placeat soluta?";
+  let ancientIdentity = document.getElementById("identity");
+  ancientIdentity.innerHTML = "Mark Edwards";
     
   
     
